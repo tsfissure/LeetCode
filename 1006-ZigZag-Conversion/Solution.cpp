e1d21ab9a624b11390a507bbeae09cb01811ab9b
tsFissure <tsfissure@qq.com>
2015-11-04 13:31:01 +0800

    Create Solution.cpp
    Solution

---
+++ 1006-ZigZag-Conversion/Solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+	static int computeMoveDistance(int iCurLine, int iNumRows, bool bIsUp) {
+		return bIsUp ? iCurLine << 1 : (iNumRows - iCurLine - 1) << 1;
+	}
+
+	string convert(string s, int numRows) {
+		if (numRows < 2) return s;
+		string kResult = "";
+
+		for (int i = 0; i < numRows; ++i) {
+			bool bUp = false;
+			for (int iMoveDis, j = i; j < (int)s.size(); j += iMoveDis) {
+				iMoveDis = computeMoveDistance(i, numRows, bUp);
+				if (iMoveDis > 0) kResult += s[j];
+				bUp ^= true;
+			}
+		}
+		return kResult;
+	}
+};
